57b38461e5fb6df847eec78834f71f03cd4fbf88
Heremanu <heremanu.besset@edu.ece.fr>
2025-12-02 15:34:40 +0100

    Remodelage du devellopement du fichier utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,66 +1,103 @@
-
-// FICHIER: utils.c
-
-
+#include "utils.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "../include/utils.h"
 
 void vider_buffer() {
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+    // Lire et ignorer tous les caractères jusqu'au prochain '\n' ou EOF
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
 }
 
 void obtenir_date_actuelle(char* date) {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    sprintf(date, "%04d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+    // Format "JJ/MM/AAAA"
+    strftime(date, MAX_DATE, "%d/%m/%Y", &tm);
 }
 
 void ajouter_jours(const char* date_depart, int jours, char* date_arrivee) {
-    struct tm tm = {0};
-    int annee, mois, jour;
-
-    sscanf(date_depart, "%d-%d-%d", &annee, &mois, &jour);
-
-    tm.tm_year = annee - 1900;
-    tm.tm_mon = mois - 1;
-    tm.tm_mday = jour + jours;
-
-    mktime(&tm);
-
-    sprintf(date_arrivee, "%04d-%02d-%02d",
-            tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+    int jour, mois, an;
+    if (sscanf(date_depart, "%d/%d/%d", &jour, &mois, &an) != 3) {
+        // format invalide, on met une date vide
+        strcpy(date_arrivee, "");
+        return;
+    }
+    struct tm tm_date = {0};
+    tm_date.tm_mday = jour;
+    tm_date.tm_mon = mois - 1;
+    tm_date.tm_year = an - 1900;
+    // Fixer l'heure à midi pour limiter les effets des changements d'heure (été/hiver)
+    tm_date.tm_hour = 12;
+    tm_date.tm_min = 0;
+    tm_date.tm_sec = 0;
+    tm_date.tm_isdst = -1;
+    time_t time_date = mktime(&tm_date);
+    if (time_date == -1) {
+        // Erreur lors de la conversion
+        strcpy(date_arrivee, "");
+        return;
+    }
+    // Ajouter les jours en secondes
+    time_date += (time_t)jours * 24 * 3600;
+    struct tm *tm_res = localtime(&time_date);
+    strftime(date_arrivee, MAX_DATE, "%d/%m/%Y", tm_res);
 }
 
 int comparer_dates(const char* date1, const char* date2) {
-    return strcmp(date1, date2);
+    int j1, m1, a1;
+    int j2, m2, a2;
+    if (sscanf(date1, "%d/%d/%d", &j1, &m1, &a1) != 3) return 0;
+    if (sscanf(date2, "%d/%d/%d", &j2, &m2, &a2) != 3) return 0;
+    if (a1 < a2) return -1;
+    if (a1 > a2) return 1;
+    // années égales
+    if (m1 < m2) return -1;
+    if (m1 > m2) return 1;
+    // mois égaux
+    if (j1 < j2) return -1;
+    if (j1 > j2) return 1;
+    return 0;
 }
 
 int calculer_jours_difference(const char* date1, const char* date2) {
+    int j1, m1, a1;
+    int j2, m2, a2;
+    if (sscanf(date1, "%d/%d/%d", &j1, &m1, &a1) != 3) return 0;
+    if (sscanf(date2, "%d/%d/%d", &j2, &m2, &a2) != 3) return 0;
     struct tm tm1 = {0}, tm2 = {0};
-    int a1, m1, j1, a2, m2, j2;
-
-    sscanf(date1, "%d-%d-%d", &a1, &m1, &j1);
-    sscanf(date2, "%d-%d-%d", &a2, &m2, &j2);
-
-    tm1.tm_year = a1 - 1900;
-    tm1.tm_mon = m1 - 1;
     tm1.tm_mday = j1;
-
-    tm2.tm_year = a2 - 1900;
-    tm2.tm_mon = m2 - 1;
+    tm1.tm_mon = m1 - 1;
+    tm1.tm_year = a1 - 1900;
+    tm1.tm_hour = 12; tm1.tm_min = 0; tm1.tm_sec = 0;
+    tm1.tm_isdst = -1;
     tm2.tm_mday = j2;
-
-    time_t t1 = mktime(&tm1);
-    time_t t2 = mktime(&tm2);
-
-    return (int)difftime(t2, t1) / (60 * 60 * 24);
+    tm2.tm_mon = m2 - 1;
+    tm2.tm_year = a2 - 1900;
+    tm2.tm_hour = 12; tm2.tm_min = 0; tm2.tm_sec = 0;
+    tm2.tm_isdst = -1;
+    time_t time1 = mktime(&tm1);
+    time_t time2 = mktime(&tm2);
+    if (time1 == -1 || time2 == -1) {
+        return 0;
+    }
+    double diff_seconds = difftime(time2, time1);
+    // Arrondir à l'entier le plus proche (pour compenser d'éventuels décalages horaires)
+    int diff_days;
+    if (diff_seconds >= 0) {
+        diff_days = (int)((diff_seconds + 43200) / 86400);
+    } else {
+        diff_days = (int)((diff_seconds - 43200) / 86400);
+    }
+    return diff_days;
 }
 
 void pause_ecran() {
-    printf("\nAppuyez sur Entree pour continuer...");
+    printf("Appuyez sur Entrée pour continuer...");
+    fflush(stdout);
     vider_buffer();
     getchar();
 }
